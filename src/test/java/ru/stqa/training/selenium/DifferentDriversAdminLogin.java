8ba75566c60cbb860a051bf5d6761c2f557a412e
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2017-02-02 22:00:40 +0300

    launch Firefox ESR 45.7.0

--- src/test/java/ru/stqa/training/selenium/DifferentDriversAdminLogin.java
+++ src/test/java/ru/stqa/training/selenium/DifferentDriversAdminLogin.java
@@ -4,10 +4,18 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.File;
 
 /**
  * Created by ilya on 25/01/2017.
@@ -22,13 +30,28 @@ public class DifferentDriversAdminLogin {
     //chromedriver
     @Test
     public void tryChromeDriver(){
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-fullscreen");
+        driver = new ChromeDriver(chromeOptions);
     }
 
     //geckodriver
     @Test
     public void tryFirefoxDriver(){
         driver = new FirefoxDriver();
+    }
+
+    //Firefox ESR 45.7.0
+    @Test
+    public void tryFirefoxESR(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE, false);
+
+        driver = new FirefoxDriver(
+                new FirefoxBinary(new File("/Applications/Firefox 2.app/Contents/MacOS/firefox-bin")),
+                new FirefoxProfile(),
+                caps);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
     }
 
     //Safari > Preferences, click Advanced, then select “Show Develop menu in menu bar.”
