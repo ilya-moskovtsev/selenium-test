f3e206dbc9be4a7b9966a66bba8b1d4d46ab84f9
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2017-03-02 21:36:53 +0300

    try Page Factory

--- src/main/java/test/utils/CustomersListPage.java
+++ src/main/java/test/utils/CustomersListPage.java
@@ -2,7 +2,11 @@ package test.utils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
 import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
@@ -14,6 +18,7 @@ public class CustomersListPage extends Page {
 
     public CustomersListPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public CustomersListPage open() {
@@ -21,8 +26,11 @@ public class CustomersListPage extends Page {
         return this;
     }
 
+    @FindBy(css = "table.dataTable tr.row")
+    private List<WebElement> customerRows;
+
     public Set<String> getCustomerIds() {
-        return driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
+        return customerRows.stream()
                 .map(e -> e.findElements(By.tagName("td")).get(2).getText())
                 .collect(toSet());
     }
